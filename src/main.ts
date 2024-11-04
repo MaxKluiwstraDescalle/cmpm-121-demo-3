41e91a3bf04a0b0a3ffb002a4195e1a0675f2c18
Max-Emlien Kluiwstra-Desaclle <91291216+MaxKluiwstraDescalle@users.noreply.github.com>
2024-11-04 09:56:56 -0800

    D3.a has been complete

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,105 @@
-// src/main.ts
+import leaflet from "leaflet";
+import "leaflet/dist/leaflet.css";
+import "./style.css";
+import "./leafletWorkaround.ts";
+import luck from "./luck.ts";
 
-const button = document.createElement("button");
-button.textContent = "Click me";
-button.addEventListener("click", () => {
-  alert("You clicked the button!");
+const img = document.createElement("img");
+img.src = import.meta.resolve("../marker.png");
+document.body.appendChild(img);
+
+const OAKES_CLASSROOM = leaflet.latLng(36.98949379578401, -122.06277128548504);
+
+const GAMEPLAY_ZOOM_LEVEL = 19;
+const TILE_DEGREES = 1e-4;
+const NEIGHBORHOOD_SIZE = 8;
+const CACHE_SPAWN_PROBABILITY = 0.1;
+
+const map = leaflet.map(document.getElementById("map")!, {
+  center: OAKES_CLASSROOM,
+  zoom: GAMEPLAY_ZOOM_LEVEL,
+  minZoom: GAMEPLAY_ZOOM_LEVEL,
+  maxZoom: GAMEPLAY_ZOOM_LEVEL,
+  zoomControl: false,
+  scrollWheelZoom: false,
 });
 
-document.body.appendChild(button);
+leaflet
+  .tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    maxZoom: 19,
+    attribution:
+      '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+  })
+  .addTo(map);
+
+const playerMarker = leaflet.marker(OAKES_CLASSROOM);
+playerMarker.bindTooltip("That's you!");
+playerMarker.addTo(map);
+
+let playerPoints = 0;
+const statusPanel = document.querySelector<HTMLDivElement>("#statusPanel")!;
+statusPanel.innerHTML = "No points yet...";
+
+const cacheCoins = new Map<string, number>();
+
+function spawnCache(i: number, j: number) {
+  const origin = OAKES_CLASSROOM;
+  const bounds = leaflet.latLngBounds([
+    [origin.lat + i * TILE_DEGREES, origin.lng + j * TILE_DEGREES],
+    [origin.lat + (i + 1) * TILE_DEGREES, origin.lng + (j + 1) * TILE_DEGREES],
+  ]);
+
+  const rect = leaflet.rectangle(bounds);
+  rect.addTo(map);
+
+  rect.bindPopup(() => {
+    const cacheKey = `${i},${j}`;
+    let coins = cacheCoins.get(cacheKey) ??
+      Math.floor(luck([i, j, "coins"].toString()) * 10);
+    cacheCoins.set(cacheKey, coins);
+
+    const popupDiv = document.createElement("div");
+    popupDiv.innerHTML = `
+      <div>There is a cache here at "${i},${j}". It has <span id="coins">${coins}</span> coins.</div>
+      <button id="collect">Collect</button>
+      <button id="deposit">Deposit</button>`;
+
+    popupDiv.querySelector<HTMLButtonElement>("#collect")!.addEventListener(
+      "click",
+      () => {
+        if (coins > 0) {
+          coins--;
+          playerPoints++;
+          cacheCoins.set(cacheKey, coins);
+          popupDiv.querySelector<HTMLSpanElement>("#coins")!.innerHTML = coins
+            .toString();
+          statusPanel.innerHTML = `${playerPoints} points accumulated`;
+        }
+      },
+    );
+
+    popupDiv.querySelector<HTMLButtonElement>("#deposit")!.addEventListener(
+      "click",
+      () => {
+        if (playerPoints > 0) {
+          coins++;
+          playerPoints--;
+          cacheCoins.set(cacheKey, coins);
+          popupDiv.querySelector<HTMLSpanElement>("#coins")!.innerHTML = coins
+            .toString();
+          statusPanel.innerHTML = `${playerPoints} points accumulated`;
+        }
+      },
+    );
+
+    return popupDiv;
+  });
+}
+
+for (let i = -NEIGHBORHOOD_SIZE; i < NEIGHBORHOOD_SIZE; i++) {
+  for (let j = -NEIGHBORHOOD_SIZE; j < NEIGHBORHOOD_SIZE; j++) {
+    if (luck([i, j].toString()) < CACHE_SPAWN_PROBABILITY) {
+      spawnCache(i, j);
+    }
+  }
+}
